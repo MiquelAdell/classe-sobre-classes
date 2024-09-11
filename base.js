a35bef2f9362e051d4a4886efd8edfdae54db7a2
Miquel Adell <miquel.adell@gmail.com>
2024-09-11 16:14:43 +0200

    afegeix herencia

--- base.js
+++ base.js
@@ -7,8 +7,29 @@ function clickBotoMoto(){
 function ready(){
 }
 
+function say(what){
+    const element = document.getElementById("balloon");
+    element.innerHTML = what;
+}
+
 
 document.addEventListener("DOMContentLoaded", function(e) {
+    
+    document.body.innerHTML = `<div class="holder">
+            <div id="canvas"></div> 
+            <div class="buttons">
+                <button id="cotxe">Cotxe</button>
+                <button id="moto">Moto</button>
+            </div>
+            <div id="balloon"></div>
+        </div>`;
+    
+    if(false == botoCotxeEnabled){
+        document.getElementById('cotxe').classList.add("disabled");    
+    }
+    if(false == botoMotoEnabled){
+        document.getElementById('moto').classList.add("disabled");    
+    }
     ready();
     document.getElementById('cotxe').addEventListener('click',function(){
         clickBotoCotxe();
